d85f78f2954ef4676276b6a21a5a89ec2a974e59
gh api Julien VINCENS julien.vincens@hotmail.fr <julien.vincens@hotmail.fr>
2025-11-28 11:39:12 +0100

    Titre et contenu de la recette

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -71,4 +71,10 @@ class Recipe < ApplicationRecord
 
     </details>
   MARKDOWN"
+
+  def find_name
+    doc = Nokogiri::HTML(self.content)
+    title = doc.at_css("#recipe-title")&.text&.strip.sub(/^#\s*/, "")
+  end
+
 end
